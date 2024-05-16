52536184beb00ced8f38434a53b48c490c8537b0
Daniel Diaz <ddiaz006@ucr.edu>
2024-05-16 17:47:45 -0500

    adding Extended BParking analyzer for fakeRate study

---
+++ include/TreeMuonSystemBParkingExt.h
@@ -0,0 +1,354 @@
+// Class to manage files for b-tag scale factors, lepton scale factors, pileup weights, and other information
+#ifndef TreeMuonSystemBParkingExt_H
+#define TreeMuonSystemBParkingExt_H
+
+#define N_MAX_LEPTONS 100
+#define N_MAX_TRACKS 2000
+#define N_MAX_JETS 100
+#define N_MAX_CSC 200
+#define N_MAX_CSCRECHITS 5000
+#define N_MAX_DTRECHITS 20000
+#define NTriggersMAX 1201 // Number of trigger in the .dat file
+#define N_CSC_CUT 20
+#define JET_PT_CUT 10
+#define MUON_PT_CUT 20
+#define N_MAX_GPARTICLES 5000
+#define MAX_MuonHLTFilters 100
+
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+#include "assert.h"
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH2D.h"
+#include "TRandom.h"
+#include "FactorizedJetCorrector.h"
+#include "JetCorrectorParameters.h"
+#include "JetCorrectionUncertainty.h"
+#include "SimpleJetResolution.h"
+#include "BTagCalibrationStandalone.h"
+#include "TTree.h"
+#include "DBSCAN.h"
+
+#include "RazorAnalyzer.h"
+
+#include "RazorHelper.h"
+
+class TreeMuonSystemBParkingExt
+{
+
+public:
+  TreeMuonSystemBParkingExt();
+  ~TreeMuonSystemBParkingExt();
+  TTree *tree_;
+  TFile *f_;
+
+  UInt_t  runNum, lumiSec, evtNum, MC_condition, npv, npu;
+
+  float rho, met, metPhi, metEENoise, metPhiEENoise, pileupWeight, pileupWeightUp, pileupWeightDown;
+
+
+  bool Flag2_all;
+
+  float genWeight;
+
+  //gLLP
+  float gLLP_csc;
+  float gLLP_dt;
+  float gLLP_beta;
+  float gLLP_eta;
+  float gLLP_phi;
+  float gLLP_e;
+  float gLLP_pt;
+  float gLLP_ctau;
+  float gLLP_decay_vertex_r;
+  float gLLP_decay_vertex_x;
+  float gLLP_decay_vertex_y;
+  float gLLP_decay_vertex_z;
+
+  int nGenParticles;
+  float gParticleE[N_MAX_GPARTICLES];
+  float gParticlePt[N_MAX_GPARTICLES];
+  float gParticleEta[N_MAX_GPARTICLES];
+  float gParticlePhi[N_MAX_GPARTICLES];
+  int gParticleMotherId[N_MAX_GPARTICLES];
+  int gParticleMotherIndex[N_MAX_GPARTICLES];
+  int gParticleId[N_MAX_GPARTICLES];
+ 
+  int nTracks;
+  float track_Pt[N_MAX_TRACKS];
+  float track_Eta[N_MAX_TRACKS];
+  float track_Phi[N_MAX_TRACKS];
+ 
+  int nLeptons;
+  float lepE[N_MAX_LEPTONS];
+  float lepPt[N_MAX_LEPTONS];
+  float lepEta[N_MAX_LEPTONS];
+  float lepPhi[N_MAX_LEPTONS];
+  int  lepPdgId[N_MAX_LEPTONS];
+  float lepDZ[N_MAX_LEPTONS];
+  float lepDXY[N_MAX_LEPTONS];
+  float lepDXYErr[N_MAX_LEPTONS];
+  float lepSF[N_MAX_LEPTONS];
+  float lepSFup[N_MAX_LEPTONS];
+  float lepSFdn[N_MAX_LEPTONS];
+  bool lepLooseId[N_MAX_LEPTONS];
+  bool lepTightId[N_MAX_LEPTONS];
+  bool lepPassLooseIso[N_MAX_LEPTONS];
+  bool lepPassTightIso[N_MAX_LEPTONS];
+  bool lepPassVTightIso[N_MAX_LEPTONS];
+  bool lepPassVVTightIso[N_MAX_LEPTONS];
+  unsigned int lepMuonType[N_MAX_LEPTONS];//only assigned for muons
+  unsigned int lepMuonQuality[N_MAX_LEPTONS];//only assigned for muons
+  //bool lep_passSingleMuTagFilter[N_MAX_LEPTONS];//only assigned for muons
+  bool  lepMuon_passHLTFilter[N_MAX_LEPTONS][MAX_MuonHLTFilters];
+
+  int nJets;
+  float jetE[N_MAX_JETS];
+  float jetPt[N_MAX_JETS];
+  float jetEta[N_MAX_JETS];
+  float jetPhi[N_MAX_JETS];
+  float jetCISV[N_MAX_JETS];
+  bool jetTightPassId[N_MAX_JETS];
+  bool HLTDecision[NTriggersMAX];
+
+  //csc
+  int           nCscRechits;
+  int           nCscRings;
+
+  int           nDtRechits;
+  int           nDtRings;
+  int           nDtStations25;
+  int           nDtWheels25;
+
+  int           nDtRechitClusters;
+  float         dtRechitClusterMaxDPhi[N_MAX_CSC];
+  bool          dtRechitClusterOverlap[N_MAX_CSC];
+  int           dtRechitClusterMaxDPhi_index[N_MAX_CSC];
+
+  int           dtRechitClusterNSegStation1[N_MAX_CSC];
+  int           dtRechitClusterNSegStation2[N_MAX_CSC];
+  int           dtRechitClusterNSegStation3[N_MAX_CSC];
+  int           dtRechitClusterNSegStation4[N_MAX_CSC];
+  int           dtRechitClusterNOppositeSegStation1[N_MAX_CSC];
+  int           dtRechitClusterNOppositeSegStation2[N_MAX_CSC];
+  int           dtRechitClusterNOppositeSegStation3[N_MAX_CSC];
+  int           dtRechitClusterNOppositeSegStation4[N_MAX_CSC];
+  int           dtRechitClusterNSegmentStation1[N_MAX_CSC];
+  int           dtRechitClusterNSegmentStation2[N_MAX_CSC];
+  int           dtRechitClusterNSegmentStation3[N_MAX_CSC];
+  int           dtRechitClusterNSegmentStation4[N_MAX_CSC];
+
+  float cscRechitClusterMatchedTrackSumPt_0p2[N_MAX_CSC];
+  float cscRechitClusterMatchedTrackLeadPt_0p2[N_MAX_CSC];
+  int   cscRechitClusterMatchedTrackSize_0p2[N_MAX_CSC];
+
+  float cscRechitClusterMatchedTrackSumPt_0p3[N_MAX_CSC];
+  float cscRechitClusterMatchedTrackLeadPt_0p3[N_MAX_CSC];
+  int   cscRechitClusterMatchedTrackSize_0p3[N_MAX_CSC];
+
+  float cscRechitClusterMatchedTrackSumPt_0p4[N_MAX_CSC];
+  float cscRechitClusterMatchedTrackLeadPt_0p4[N_MAX_CSC];
+  int   cscRechitClusterMatchedTrackSize_0p4[N_MAX_CSC];
+
+  float cscRechitClusterMatchedTrackSumPt_0p5[N_MAX_CSC];
+  float cscRechitClusterMatchedTrackLeadPt_0p5[N_MAX_CSC];
+  int   cscRechitClusterMatchedTrackSize_0p5[N_MAX_CSC];
+
+  float dtRechitClusterMatchedTrackSumPt_0p2[N_MAX_CSC];
+  float dtRechitClusterMatchedTrackLeadPt_0p2[N_MAX_CSC];
+  int   dtRechitClusterMatchedTrackSize_0p2[N_MAX_CSC];
+
+  float dtRechitClusterMatchedTrackSumPt_0p3[N_MAX_CSC];
+  float dtRechitClusterMatchedTrackLeadPt_0p3[N_MAX_CSC];
+  int   dtRechitClusterMatchedTrackSize_0p3[N_MAX_CSC];
+
+  float dtRechitClusterMatchedTrackSumPt_0p4[N_MAX_CSC];
+  float dtRechitClusterMatchedTrackLeadPt_0p4[N_MAX_CSC];
+  int   dtRechitClusterMatchedTrackSize_0p4[N_MAX_CSC];
+
+  float dtRechitClusterMatchedTrackSumPt_0p5[N_MAX_CSC];
+  float dtRechitClusterMatchedTrackLeadPt_0p5[N_MAX_CSC];
+  int   dtRechitClusterMatchedTrackSize_0p5[N_MAX_CSC];
+
+  float cscRechitClusterMatchedTrackSumPt_trk_pos_0p2[N_MAX_CSC];
+  float cscRechitClusterMatchedTrackLeadPt_trk_pos_0p2[N_MAX_CSC];
+  int   cscRechitClusterMatchedTrackSize_trk_pos_0p2[N_MAX_CSC];
+
+  float cscRechitClusterMatchedTrackSumPt_trk_pos_0p3[N_MAX_CSC];
+  float cscRechitClusterMatchedTrackLeadPt_trk_pos_0p3[N_MAX_CSC];
+  int   cscRechitClusterMatchedTrackSize_trk_pos_0p3[N_MAX_CSC];
+
+  float cscRechitClusterMatchedTrackSumPt_trk_pos_0p4[N_MAX_CSC];
+  float cscRechitClusterMatchedTrackLeadPt_trk_pos_0p4[N_MAX_CSC];
+  int   cscRechitClusterMatchedTrackSize_trk_pos_0p4[N_MAX_CSC];
+
+  float cscRechitClusterMatchedTrackSumPt_trk_pos_0p5[N_MAX_CSC];
+  float cscRechitClusterMatchedTrackLeadPt_trk_pos_0p5[N_MAX_CSC];
+  int   cscRechitClusterMatchedTrackSize_trk_pos_0p5[N_MAX_CSC];
+
+  float dtRechitClusterMatchedTrackSumPt_trk_pos_0p2[N_MAX_CSC];
+  float dtRechitClusterMatchedTrackLeadPt_trk_pos_0p2[N_MAX_CSC];
+  int   dtRechitClusterMatchedTrackSize_trk_pos_0p2[N_MAX_CSC];
+
+  float dtRechitClusterMatchedTrackSumPt_trk_pos_0p3[N_MAX_CSC];
+  float dtRechitClusterMatchedTrackLeadPt_trk_pos_0p3[N_MAX_CSC];
+  int   dtRechitClusterMatchedTrackSize_trk_pos_0p3[N_MAX_CSC];
+
+  float dtRechitClusterMatchedTrackSumPt_trk_pos_0p4[N_MAX_CSC];
+  float dtRechitClusterMatchedTrackLeadPt_trk_pos_0p4[N_MAX_CSC];
+  int   dtRechitClusterMatchedTrackSize_trk_pos_0p4[N_MAX_CSC];
+
+  float dtRechitClusterMatchedTrackSumPt_trk_pos_0p5[N_MAX_CSC];
+  float dtRechitClusterMatchedTrackLeadPt_trk_pos_0p5[N_MAX_CSC];
+  int   dtRechitClusterMatchedTrackSize_trk_pos_0p5[N_MAX_CSC];
+
+  int           dtRechitCluster_match_MB1hits_0p4[N_MAX_CSC];
+  int           dtRechitCluster_match_MB1hits_0p5[N_MAX_CSC];
+  int           dtRechitCluster_match_MB1hits_cosmics_plus[N_MAX_CSC];
+  int           dtRechitCluster_match_MB1hits_cosmics_minus[N_MAX_CSC];
+  int           dtRechitCluster_match_MB1Seg_0p4[N_MAX_CSC];
+  int           dtRechitCluster_match_MB1Seg_0p5[N_MAX_CSC];
+
+  int           dtRechitCluster_match_RPCBx_dPhi0p5[N_MAX_CSC];
+  int           dtRechitCluster_match_RB1_0p4[N_MAX_CSC];
+  int           dtRechitCluster_match_RB1_dPhi0p5[N_MAX_CSC];
+
+  float         dtRechitClusterJetVetoEta[N_MAX_CSC];
+  float         dtRechitClusterJetVetoPhi[N_MAX_CSC];
+  float         dtRechitClusterJetVetoPt[N_MAX_CSC];
+  float         dtRechitClusterJetVetoE[N_MAX_CSC];
+
+  float         dtRechitClusterGenMuonVetoPt[N_MAX_CSC];
+  float         dtRechitClusterGenMuonVetoPt_dR0p8[N_MAX_CSC];
+  float         dtRechitClusterMuonVetoPt[N_MAX_CSC];
+  float         dtRechitClusterMuonVetoE[N_MAX_CSC];
+  float         dtRechitClusterMuonVetoPhi[N_MAX_CSC];
+  float         dtRechitClusterMuonVetoEta[N_MAX_CSC];
+  bool          dtRechitClusterMuonVetoLooseId[N_MAX_CSC];
+  bool          dtRechitClusterMuonVetoGlobal[N_MAX_CSC];
+  float         dtRechitClusterMetEENoise_dPhi[N_MAX_CSC];
+  float         dtRechitCluster_match_gLLP_deltaR[N_MAX_CSC];
+
+//---- only two actually being used, check
+  float         dtRechitCluster_match_RPCTime_dPhi0p5[N_MAX_CSC];
+  float         dtRechitCluster_match_RPCTimeSpread_dPhi0p5[N_MAX_CSC];
+  int           dtRechitCluster_match_RPChits_dPhi0p5[N_MAX_CSC];
+  float         dtRechitCluster_match_RPCTime_dR0p4[N_MAX_CSC];
+  float         dtRechitCluster_match_RPCTimeSpread_dR0p4[N_MAX_CSC];
+  int           dtRechitCluster_match_RPChits_dR0p4[N_MAX_CSC];
+  float         dtRechitCluster_match_RPCTime_sameStation_dR0p4[N_MAX_CSC];
+  float         dtRechitCluster_match_RPCTimeSpread_sameStation_dR0p4[N_MAX_CSC];
+  int           dtRechitCluster_match_RPChits_sameStation_dR0p4[N_MAX_CSC];
+
+  float         dtRechitClusterX[N_MAX_CSC];   //[nCsc]
+  float         dtRechitClusterY[N_MAX_CSC];   //[nCsc]
+  float         dtRechitClusterZ[N_MAX_CSC];   //[nCsc]
+
+  int           dtRechitClusterWheel[N_MAX_CSC];
+
+  float         dtRechitClusterEta[N_MAX_CSC];   //[nCsc]
+  float         dtRechitClusterPhi[N_MAX_CSC];   //[nCsc]
+  int           dtRechitClusterSize[N_MAX_CSC];
+  int           dtRechitClusterNoiseHit[N_MAX_CSC];
+  int           dtRechitClusterNoiseHitStation1[N_MAX_CSC];
+  int           dtRechitClusterNoiseHitStation2[N_MAX_CSC];
+  int           dtRechitClusterNoiseHitStation3[N_MAX_CSC];
+  int           dtRechitClusterNoiseHitStation4[N_MAX_CSC];
+  float         dtRechitClusterMe11Ratio[N_MAX_CSC];
+  float         dtRechitClusterMe12Ratio[N_MAX_CSC];
+
+
+  float         dtRechitClusterMaxStationRatio[N_MAX_CSC];   //[nCsc]
+  int           dtRechitClusterMaxStation[N_MAX_CSC];   //[nCsc]
+  int           dtRechitClusterNStation[N_MAX_CSC];
+  int           dtRechitClusterNStation5[N_MAX_CSC];
+  int           dtRechitClusterNStation10[N_MAX_CSC];
+  int           dtRechitClusterNStation10perc[N_MAX_CSC];
+  float         dtRechitClusterAvgStation[N_MAX_CSC];
+  float         dtRechitClusterAvgStation5[N_MAX_CSC];
+  float         dtRechitClusterAvgStation10[N_MAX_CSC];
+  float         dtRechitClusterAvgStation10perc[N_MAX_CSC];
+  float         dtRechitClusterMaxChamberRatio[N_MAX_CSC];   //[nCsc]
+  int           dtRechitClusterMaxChamber[N_MAX_CSC];   //[nCsc]
+  int           dtRechitClusterNChamber[N_MAX_CSC];
+
+  int           nCscRechitClusters;
+
+  int           cscRechitCluster_match_dtSeg_0p4[N_MAX_CSC];
+  int           cscRechitCluster_match_MB1Seg_0p4[N_MAX_CSC];
+  int           cscRechitCluster_match_RB1_0p4[N_MAX_CSC];
+  int           cscRechitCluster_match_RE12_0p4[N_MAX_CSC];
+ 
+  float         cscRechitClusterX[N_MAX_CSC];   //[nCsc]
+  float         cscRechitClusterY[N_MAX_CSC];   //[nCsc]
+  float         cscRechitClusterZ[N_MAX_CSC];   //[nCsc]
+  float         cscRechitClusterTime[N_MAX_CSC];   //[nCsc]
+  float         cscRechitClusterTimeTotal[N_MAX_CSC];
+  float         cscRechitClusterTimeWeighted[N_MAX_CSC];
+
+  float         cscRechitClusterTimeSpread[N_MAX_CSC];
+  float         cscRechitClusterTimeSpreadWeighted[N_MAX_CSC];
+  float         cscRechitClusterTimeSpreadWeightedAll[N_MAX_CSC];
+
+  float         cscRechitClusterGenMuonDeltaR[N_MAX_CSC];
+
+  float         cscRechitClusterEta[N_MAX_CSC];   //[nCsc]
+  float         cscRechitClusterPhi[N_MAX_CSC];   //[nCsc]
+  int           cscRechitClusterSize[N_MAX_CSC];
+  float         cscRechitClusterMe11Ratio[N_MAX_CSC];
+  float         cscRechitClusterMe12Ratio[N_MAX_CSC];
+
+//----only using NStation10 and AvgStation10, can delete corresponding dt vars too
+  float         cscRechitClusterMaxStationRatio[N_MAX_CSC];   //[nCsc]
+  int           cscRechitClusterMaxStation[N_MAX_CSC];   //[nCsc]
+  int           cscRechitClusterNStation[N_MAX_CSC];
+  int           cscRechitClusterNStation5[N_MAX_CSC];
+  int           cscRechitClusterNStation10[N_MAX_CSC];
+  int           cscRechitClusterNStation10perc[N_MAX_CSC];
+  float          cscRechitClusterAvgStation[N_MAX_CSC];
+  float          cscRechitClusterAvgStation5[N_MAX_CSC];
+  float          cscRechitClusterAvgStation10[N_MAX_CSC];
+  float          cscRechitClusterAvgStation10perc[N_MAX_CSC];
+  float         cscRechitClusterMaxChamberRatio[N_MAX_CSC];   //[nCsc]
+  int           cscRechitClusterMaxChamber[N_MAX_CSC];   //[nCsc]
+  int           cscRechitClusterNChamber[N_MAX_CSC];
+
+
+  int           cscRechitClusterNRechitChamberPlus11[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberPlus12[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberPlus13[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberPlus21[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberPlus22[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberPlus31[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberPlus32[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberPlus41[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberPlus42[N_MAX_CSC];
+
+  int           cscRechitClusterNRechitChamberMinus11[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberMinus12[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberMinus13[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberMinus21[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberMinus22[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberMinus31[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberMinus32[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberMinus41[N_MAX_CSC];
+  int           cscRechitClusterNRechitChamberMinus42[N_MAX_CSC];
+
+  float         cscRechitClusterJetVetoPt[N_MAX_CSC];
+
+  float         cscRechitClusterGenMuonVetoPt[N_MAX_CSC];
+  float         cscRechitClusterGenMuonVetoPt_dR0p8[N_MAX_CSC];
+
+  float         cscRechitClusterMuonVetoPt[N_MAX_CSC];
+  float           cscRechitClusterMetEENoise_dPhi[N_MAX_CSC];
+float           cscRechitCluster_match_gLLP_deltaR[N_MAX_CSC];
+
+  void InitVariables();
+  void InitTree();
+  void LoadTree(const char* file);
+  void CreateTree();
+
+};
+#endif
---
+++ scripts_condor/submitJobs_muonsystem_bparking_ext.sh
@@ -0,0 +1,266 @@
+#!/bin/bash
+
+#(NOT ADDED YET)If you want to send to an eos directory put the LFN here
+sendToEOS=true
+eosdir="/store/group/lpclonglived/B-ParkingLLPs/"
+eosprefix="/eos/uscms"
+
+doSubmit=true
+version="V1p17_0" #"V1p19_10"
+isData="yes" #"yes" or "no"
+options=-1
+#outputfilename=""
+#label="BParking_Source2018"
+#label="Razor2016_07Aug2017Rereco"
+#label="Razor2017_17Nov2017Rereco"
+label="Razor2018_17SeptEarlyReReco"
+filesPerJob=5   #40 good for signal, 5 good for Data
+#-1 to do all
+maxJobs=-1 #-1
+
+mclistdir=${CMSSW_BASE}/src/llp_analyzer/lists/displacedJetMuonNtuple/V1p19/MC_Fall18/v1/sixie
+#datalistdir=${CMSSW_BASE}/src/llp_analyzer/lists//displacedJetMuonNtuple/V1p19/Data2018_UL
+datalistdir=${CMSSW_BASE}/src/llp_analyzer/lists/displacedJetMuonNtuple/V1p17/Data2018ABC_AOD/v5/sixie/SingleMuon
+subdir=${CMSSW_BASE}/src/llp_analyzer/scripts_condor/gitignore/$version
+
+#for assigning indices to files after splitting
+padding=7
+printf "Version: ${version}\n"
+
+cd ${CMSSW_BASE}/..
+tar --exclude-caches-all --exclude-vcs -zcf CMSSW_9_4_4.tar.gz -C CMSSW_9_4_4/.. CMSSW_9_4_4 --exclude=src --exclude=tmp --exclude="*.root"
+cd -
+cp ${CMSSW_BASE}/../CMSSW_9_4_4.tar.gz .
+
+samples=(  \
+ "SingleMuon" \
+)
+
+
+# "ParkingBPH1_2018C"      \
+# "ParkingBPH2_2018C"      \
+# "ParkingBPH3_2018C"      \
+# "ParkingBPH4_2018C"      \
+# "ParkingBPH5_2018C"      \
+# "ParkingBPH1_2018B"      \
+# "ParkingBPH2_2018B"      \
+# "ParkingBPH3_2018B"      \
+# "ParkingBPH4_2018B"      \
+# "ParkingBPH5_2018B"      \
+# "ParkingBPH6_2018B"      \
+# "ParkingBPH1_2018D"      \
+# "ParkingBPH2_2018D"      \
+# "ParkingBPH3_2018D"      \
+# "ParkingBPH4_2018D"      \
+# "ParkingBPH5_2018D"      \
+# "ParkingBPH1_2018D"      \
+# "ParkingBPH2_2018D"      \
+# "ParkingBPH3_2018D"      \
+# "ParkingBPH4_2018D"      \
+# "ParkingBPH5_2018D"      \
+
+
+# done
+#  "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_GenOnly_mPhi0p5_ctau500" \
+#  "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_GenOnly_mPhi0p5_ctau5000" \
+#  "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_GenOnly_mPhi1p0_ctau10000" \
+#  "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_GenOnly_mPhi2p0_ctau10000" \
+#  "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_GenOnly_mPhi2p0_ctau2000" \
+#  "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_GenOnly_mPhi3p0_ctau10000" \
+#  "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_GenOnly_mPhi3p0_ctau3000" \
+#  "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_GenOnly_mPhi0p3_ctau3000" \
+# "BToKPhi_MuonGenFilter_PhiToPi0Pi0_mPhi0p3_ctau1000" \
+# "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_mPhi0p3_ctau1000" \
+# "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_mPhi0p3_ctau300" \
+# "BToKPhi_MuonGenFilter_PhiToPiPlusPiMinus_mPhi1p0_ctau1000.GenOnly" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi0p3_ctau300" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi0p3_ctau3000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi0p5_ctau500" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi0p5_ctau5000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi1p0_ctau1000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi1p0_ctau10000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi1p0_ctau300" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi2p0_ctau10000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi2p0_ctau2000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi3p0_ctau10000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi3p0_ctau3000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi0p3_ctau300" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi0p3_ctau3000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi0p5_ctau500" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi0p5_ctau5000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi1p0_ctau1000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi1p0_ctau10000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi1p0_ctau300" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi2p0_ctau10000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi2p0_ctau2000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi3p0_ctau10000" \
+# "BToKPhi_MuonLLPDecayGenFilter_PhiToPiPlusPiMinus_mPhi3p0_ctau3000" \
+ 
+mkdir -p $subdir
+makeasubmitdir () {
+ printf "Making submits for $1\n"
+ 
+ # go to the directory
+ submitdir=${subdir}/$1
+ mkdir -p ${submitdir} 
+ pushd    ${submitdir}  > /dev/null
+ printf " The directory is %s\n" $(pwd)
+ 
+ mkdir -p logs
+ 
+ # breaking up input file list
+ split -l $filesPerJob -d -a $padding --additional-suffix=.txt $3/${sample}.txt ${sample}"_"
+ mkdir -p lists
+ mv *.txt lists/
+ tar -zcf lists.tgz lists
+ 
+ # write base for submit file
+ printf "universe = vanilla\n" > submitfile
+ printf "Executable = ${CMSSW_BASE}/src/llp_analyzer/scripts_condor/runJobs_muonsystem_bparking.sh\n" >> submitfile
+ printf "Should_Transfer_Files = YES \n" >> submitfile
+ printf "WhenToTransferOutput = ON_EXIT\n" >> submitfile
+ printf "Transfer_Input_Files = ${submitdir}/lists.tgz,${CMSSW_BASE}/src/llp_analyzer/JEC.tar.gz,${CMSSW_BASE}/src/llp_analyzer/scripts_condor/CMSSW_9_4_4.tar.gz,${CMSSW_BASE}/src/llp_analyzer/bin/Runllp_MuonSystem_bparking,${CMSSW_BASE}/src/llp_analyzer/data/PileupWeights/PileupReweight_2018_BParking.root,${CMSSW_BASE}/src/llp_analyzer/data/ScaleFactors/BParking_SF.root,${CMSSW_BASE}/src/llp_analyzer/data/ScaleFactors/METTriggers_SF.root,${CMSSW_BASE}/src/llp_analyzer/data/HiggsPtWeights/ggH_HiggsPtReweight_NNLOPS.root\n" >> submitfile
+ printf "\n" >> submitfile
+ printf "notify_user = $(whoami)@cern.ch\n" >> submitfile
+ printf "x509userproxy = $X509_USER_PROXY\n" >> submitfile
+ printf "\n" >> submitfile
+ printf "Output = logs/runanalyzer_muonsystem_bparking_\$(Cluster)_\$(Process).stdout\n" >> submitfile
+ printf "Error  = logs/runanalyzer_muonsystem_bparking_\$(Cluster)_\$(Process).stderr\n" >> submitfile
+ printf "Log    = logs/runanalyzer_muonsystem_bparking_\$(Cluster)_\$(Process).log\n" >> submitfile
+
+ TheEosDir=${eosdir}/${version}/$1
+ if [ ${sendToEOS} == true ]; then 
+   if [ -d ${eosprefix}${TheEosDir} ]; then
+     eos root://cmseos.fnal.gov rm -r ${TheEosDir} 
+   fi
+   eos root://cmseos.fnal.gov mkdir -p $TheEosDir
+   echo making eos dir
+   echo ${eosprefix}$TheEosDir
+ fi
+
+ # make haddfile (make now for merging expected results)
+ haddfile="./haddit.sh"
+ printf "#!/bin/bash\n\n" > ${haddfile}    
+ # make checker
+ checkfile="./checker.sh"
+ printf "#!/bin/bash\n\n" > ${checkfile}
+ # make cleaner
+ cleaner="./cleanup.sh"
+ printf "#!/bin/bash\n\n" > ${cleaner}
+
+ jobnrlow=0
+ jobnr=0
+ haddnr=0
+
+ if [ ${sendToEOS} == true ]; then
+   # hadd command, name of the first intermediate merged file
+   printf "hadd ${eosprefix}${TheEosDir}/$1-hadd${haddnr}.root"     >>       ${haddfile}
+ else
+   # hadd command, name of the first intermediate merged file
+   printf "hadd ${submitdir}/$1-hadd${haddnr}.root"     >>       ${haddfile}
+ fi    
+
+ printf "\n" >> submitfile
+ until [ ${jobnrlow} -ge $2 ]; do
+  index=$(printf "%0*d" ${padding} ${jobnr});
+  listFile=$submitdir/${sample}"_"${index}.txt
+  printf "Arguments = $1 $index $isData $label $options $TheEosDir $sendToEOS\n" >> submitfile
+  printf "Queue\n" >> submitfile
+  printf "\n" >> submitfile
+
+  # add files to be produced to haddfiles
+  printf "\\"  >> ${haddfile}    
+
+  if [ ${sendToEOS} == true ]; then
+    printf "\n eos root://cmseos.fnal.gov rm ${TheEosDir}/$1_${index}.root"     >> ${cleaner}    
+    printf "\n ${eosprefix}${TheEosDir}/$1_${index}.root"     >> ${haddfile}    
+    # add file to checker, all histos are made at the same time, so only check one
+    printf "\n if [ ! -f ${eosprefix}${TheEosDir}/$1_${index}.root ]; then printf \" ${eosprefix}${TheEosDir}/$1_${index}.root \\n\"; fi " >> ${checkfile}
+  else 
+    printf "\n rm $(pwd)/$1_${index}.root"     >> ${cleaner}    
+    printf "\n $(pwd)/$1_${index}.root"     >> ${haddfile}    
+    # add file to checker, all histos are made at the same time, so only check one
+    printf "\n if [ ! -f $(pwd)/$1_${index}.root ]; then printf \" $(pwd)/$1_${index}.root \\n\"; fi " >> ${checkfile}
+  fi
+
+  if [ $((${jobnr}%190)) == 0 ] && [ ${jobnr} -ne 0 ]; then
+    haddnr=$(( ${haddnr} + 1 ))
+    printf "\\"  >> ${haddfile}    
+    if [ ${sendToEOS} == true ]; then
+      # hadd command, name of the nth intermediate merged file
+      printf "\n\n" >> ${haddfile}
+      printf "hadd ${eosprefix}${TheEosDir}/$1-hadd${haddnr}.root"     >>       ${haddfile}
+    else
+      # hadd command, name of the nth intermediate merged file
+      printf "\n\n" >> ${haddfile}
+      printf "hadd ${submitdir}/$1-hadd${haddnr}.root"     >>       ${haddfile}
+    fi    
+  fi
+  # increment filenumber counters
+  jobnrlow=$(( ${jobnrlow} + 1 ))
+  jobnr=$(( ${jobnr} + 1 ))
+ done # until jobnrlow > nTotalJobs
+
+ printf "\n\n\n" >> ${haddfile}
+ printf "##--Now Merge the final file \n" >> ${haddfile}    
+ # hadd command, name of final merged file
+ printf "hadd -f ${eosprefix}${TheEosDir}/$1.root"     >>       ${haddfile}
+ iter=0    
+ until [ $iter -gt $haddnr ]; do
+  printf "\\"  >> ${haddfile}    
+  if [ ${sendToEOS} == true ]; then
+    printf "\n eos root://cmseos.fnal.gov rm ${TheEosDir}/$1-hadd${iter}.root"     >> ${cleaner}    
+    printf "\n ${eosprefix}${TheEosDir}/$1-hadd${iter}.root"     >> ${haddfile}    
+  else 
+    printf "\n rm $(pwd)/$1-hadd${iter}.root"     >> ${cleaner}    
+    printf "\n $(pwd)/$1-hadd${iter}.root"     >> ${haddfile}    
+  fi
+  iter=$(( ${iter} + 1 ))
+ done
+
+
+ if [ ${doSubmit} = true ]; then
+  condor_submit submitfile
+ fi
+ 
+ popd > /dev/null
+}
+
+
+# actually call the function
+for sample in ${samples[@]}; do
+  if [[ ${sample} == "Parking"* ]]; then 
+    isData="yes"
+  else
+    isData="no"
+  fi
+
+  #data or MC?
+  listdir=./
+  if [[ "$isData" == "yes" ]];  then
+    listdir=$datalistdir
+  else
+    listdir=$mclistdir
+  fi
+  echo is data? $isData
+
+  nlines=`cat ${listdir}/${sample}.txt | wc -l`
+  njobs1=$((nlines/filesPerJob))
+  njobs2=$((nlines%filesPerJob))
+  if [ $njobs2 -gt 0 ]; then
+    njobs=$((njobs1+1))
+  else  
+    njobs=$((njobs1))
+  fi
+
+  if [ $maxJobs -eq -1 ]; then 
+    nTotalJobs=$((njobs))
+  elif [ $maxjobs -lt $njobs ]; then
+    nTotalJobs=$maxjobs
+  else
+    nTotalJobs=$((nTotalJobs+njobs))
+  fi
+  echo Total number of jobs for $sample : $nTotalJobs
+  makeasubmitdir ${sample} ${nTotalJobs} ${listdir}
+  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+done
